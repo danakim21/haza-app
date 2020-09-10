8ca5dc2e2a808aa75d41df3163a47cfe49db1925
danakim21 <danakim21@gmail.com>
2020-09-10 20:43:58 +0900

    Updates

---
+++ precache-manifest.a0d1cf344ec7b23359186d0e5963b798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa93c375455a4581679093f4e97401a",
+    "url": "/haza-app/index.html"
+  },
+  {
+    "revision": "69747d17fef8cbc87171",
+    "url": "/haza-app/static/css/main.6b36e00e.chunk.css"
+  },
+  {
+    "revision": "cd848d756babd2a01f90",
+    "url": "/haza-app/static/js/2.a060aa62.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/haza-app/static/js/2.a060aa62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69747d17fef8cbc87171",
+    "url": "/haza-app/static/js/main.9a51b465.chunk.js"
+  },
+  {
+    "revision": "31be41ac70e29523fa28",
+    "url": "/haza-app/static/js/runtime-main.87085d7c.js"
+  }
+]);
